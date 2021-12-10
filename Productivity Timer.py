541336611d8169f39711263e4d05e0324ade943d
DevRollins <93947568+DevRollins@users.noreply.github.com>
2021-12-10 00:17:10 -0500

    Update Productivity Timer.py

--- Productivity Timer.py
+++ Productivity Timer.py
@@ -1,13 +1,9 @@
 ## This is a program that will help you manage your time and hopefully aid you in increasing productivity. 
-## Start the program by welcoming the user to the program and prompting them to input how long they would like to study
-## and for how long they would like to take breaks 
 
-## Write code that doodles while you work.
 import time 
 import winsound   ## Import necessary libraries for all functionality 
 import turtle 
 from turtle import *
-import random 
 
 window = turtle.Screen()      
 window.title('Productivity Timer')      ## Create window for user input
@@ -20,7 +16,7 @@ pen.up()
 pen.setx(0)
 window.bgcolor('black')
 
-        ## Red Colors
+        ## Red Colors                       ##Declare Colors for the colorloop
 COLORS = [(1.0, 0.0, 0.0), (1.0, 0.05, 0.00,), (1.0, 0.10, 0.0), (1.0, 0.15, 0.0), (1.0, 0.20, 0.0), (1.0, 0.25, 0.0), (1.0, 0.30, 0.0),
         (1.0, 0.35, 0.0), (1.0, 0.40, 0.0), (1.0, 0.45, 0.0), (1, 0.50, 0.0), (1.0, 0.55, 0.00),
         ## Green Colors
@@ -34,70 +30,65 @@ def main(): ## Welcome user to the program while initializing variables for late
     worktime = int(window.numinput('Work', 'In minutes, how long would you like to be productive?', minval=1, maxval=9000) * 60)
     breaktime = int(window.numinput('Break', 'In minutes, how long would you like to rest between productivity?', minval=1, maxval=9000) * 60)
     loop = int(window.numinput('Loop', 'How many times would you like to loop through this?', minval=1, maxval=9000))
-    print('\nHere we go! Your productivity begins... NOW!')
 
-    while loop:
-        for i in range(loop): ## Loop that creates a countdown timer 
-            t = worktime
-            while t:
-                mins = t // 60
-                secs = t % 60 
-                timer = '{:02d}:{:02d}'.format(mins, secs) 
-                pen.sety(150)
-                pen.write('Work! Work! Work!',align='center', font=('Times New Roman', 25))
-                pen.sety(75)
-                pen.write('Time Left:', align='center', font=('Times New Roman', 25))
-                pen.sety(0)
-                pen.write(timer, align='center', font=('Times New Roman', 25))
-                time.sleep(0.5)
-                t-= 1  
-                pen.clear()
-                if t == 0:
-                    winsound.PlaySound("Notification.wav", winsound.SND_FILENAME) ## When the timer reaches zero, the 'Notification' sound will play
-            pen.write('YOU DID IT! NOW FOR A BREAK', align='center', font=('Times New Roman', 25))    ## Message notifying user when each work period is up    
-            time.sleep(2)
+    for i in range(loop): ## Loop that creates a countdown timer 
+        t = worktime
+        while t:
+            mins = t // 60
+            secs = t % 60 
+            timer = '{:02d}:{:02d}'.format(mins, secs) 
+            pen.sety(150)
+            pen.write('Work! Work! Work!',align='center', font=('Times New Roman', 25)) ##Print label for timer during each period
+            pen.sety(75)
+            pen.write('Time Left:', align='center', font=('Times New Roman', 25))
+            pen.sety(0)
+            pen.write(timer, align='center', font=('Times New Roman', 25))
+            time.sleep(0.5) ## Time blinks to reveal each new second
+            t-= 1  
             pen.clear()
-            t = breaktime  ## New timer for the break
-            while t:
-                mins = t // 60
-                secs = t % 60 
-                timer = '{:02d}:{:02d}'.format(mins, secs) 
-                pen.sety(150)
-                pen.write('Take break! You are killing it!', align='center', font=('Times New Roman', 25))
-                pen.sety(75)
-                pen.write('Time Left:', align='center', font=('Times New Roman', 25))
-                pen.sety(0)
-                pen.write(timer, align='center', font=('Times New Roman', 25))
-                time.sleep(0.5)
-                t-= 1 
-                pen.clear()
-                if t == 0:
-                    winsound.PlaySound("GetBusy.wav", winsound.SND_FILENAME)    ## New sound
-            pen.write("And your break is up! Let's go again!", align='center', font=('Times New Roman', 25))    ## Message notifying user when each break period is up.
-            break
+            if t == 0:
+                winsound.PlaySound("Notification.wav", winsound.SND_FILENAME) ## When the timer reaches zero, the 'Notification' sound will play
+        pen.write('YOU DID IT! NOW FOR A BREAK', align='center', font=('Times New Roman', 25))    ## Message notifying user when each work period is up    
+        time.sleep(2)
+        pen.clear()
+        t = breaktime  ## New timer for the break
+        while t:
+            mins = t // 60
+            secs = t % 60 
+            timer = '{:02d}:{:02d}'.format(mins, secs) 
+            pen.sety(150)
+            pen.write('Take break! You are killing it!', align='center', font=('Times New Roman', 25))
+            pen.sety(75)
+            pen.write('Time Left:', align='center', font=('Times New Roman', 25))
+            pen.sety(0)
+            pen.write(timer, align='center', font=('Times New Roman', 25))
+            time.sleep(0.5)
+            t-= 1 
+            pen.clear()
+            if t == 0:
+                winsound.PlaySound("Notification.wav", winsound.SND_FILENAME)
+        pen.write("And your break is up! Let's go again!", align='center', font=('Times New Roman', 25))    ## Message notifying user when each break period is up.
+        colorloop()
+
+    time.sleep(2)   
     turtle.Terminator() 
-    pen.write('Good job! You have completed your productivity for now!', align='center', font=('Times New Roman', 50, 'bold'))    ## Message to notify user when the entire work session is over. 
+    house()
+    pen.write('Look, I made this while you were busy.',align='center', font=('Times New Roman', 25))
+    time.sleep(5) 
     pen.clear()     ## Clear entire screen
     artist.clear()
     bye()   ## Close turtle
-           ## Declare that the window closes whenever the red x is hit
-    
-def colorloop(): 
-    try:
-            for i in range(1000):
-                    idx = int(i/10)
-                    color = COLORS[idx]
-                    artist.color(color)
-                    artist.forward(i)
-                    artist.right(98)
-                    artist.speed(0)
-            artist.setx(0)
-            artist.sety(0)
-    except IndexError:
-            turtle.reset()
-            turtle.sety(0)
-            colorloop()
-    artist.clear()
+
+def colorloop():
+    for i in range(1000):
+        idx = int(i/10)
+        color = COLORS[idx]
+        turtle.color(color)
+        turtle.forward(i)
+        turtle.right(98)
+        turtle.speed(0)
+        if IndexError:
+            break
 
 def house():  ## Use turtle to draw while the user is working. For now, it is a house on a sunny day.
     speed(0) 
